2ab0e665b2af1b0dc56fff24ba4cbb3f31e53572
Tigran Asriyan <tigran.asriyan@gmail.com>
2014-03-10 00:26:38 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,6 +11,13 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
 		end
 
 		describe "with valid information" do
@@ -24,10 +31,19 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
-		end
 		
 		it { should have_content('Sign up') }
 		it { should have_title(full_title('Sign up')) }
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_link('Sign out') }
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			end
+		end
 	end
 
 	describe "profile page" do
